47ac5842b7d2025db402bc5518cf95401d1ebf2a
Adlyn Morrison <adlynm@gmail.com>
2015-01-29 23:22:22 -0600

    beginnings of the skeleton

---
+++ web-app-skeleton/js/app/app.js
@@ -0,0 +1,16 @@
+define([
+	'app/views/app'
+	'app/views/counter'
+], function(AppView){
+
+		'use strict';
+
+		var initialize = function() {
+			var appView = new AppView();
+			$('body').append(appView.el);
+		};
+
+		return {
+			initialize: initialize,
+		}
+});
