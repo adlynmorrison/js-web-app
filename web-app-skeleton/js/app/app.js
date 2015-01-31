6a8097aaf17085df758e5e49bb5188cb4243458e
Adlyn Morrison <adlynm@gmail.com>
2015-01-30 23:35:16 -0600

    web app skeleton

--- web-app-skeleton/js/app/app.js
+++ web-app-skeleton/js/app/app.js
@@ -1,13 +1,16 @@
 define([
-	'app/views/app'
-	'app/views/counter'
-], function(AppView){
+	'app/views/app',
+	'app/routers/router'
+], function(AppView, Router){
 
 		'use strict';
 
 		var initialize = function() {
 			var appView = new AppView();
 			$('body').append(appView.el);
+			//any arguments to constructor will pass to initalize method
+			var router = new Router(appView);
+			Backbone.history.start();
 		};
 
 		return {
