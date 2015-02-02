800de0006cefcc5e86cdb3eb26390f38e1533f72
Adlyn Morrison <adlynm@gmail.com>
2015-02-01 23:16:22 -0600

    models

---
+++ web-app-models/js/app/app.js
@@ -0,0 +1,23 @@
+define([
+	'app/views/app',
+	'app/routers/router',
+	'app/models/app'
+], function(AppView, Router, AppModel){
+
+		'use strict';
+
+
+		var initialize = function() {
+			var appModel = new AppModel();
+
+			var appView = new AppView({model: appModel});
+			$('body').append(appView.el);
+			//any arguments to constructor will pass to initalize method
+			var router = new Router(appView);
+			Backbone.history.start();
+		};
+
+		return {
+			initialize: initialize,
+		}
+});
