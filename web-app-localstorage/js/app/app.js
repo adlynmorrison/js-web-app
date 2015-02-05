c5dd7f526609fa1475a3bac212bf622b956efa17
Adlyn Morrison <adlynm@gmail.com>
2015-02-04 22:22:10 -0600

    added places.js in collections and models folders

--- web-app-localstorage/js/app/app.js
+++ web-app-localstorage/js/app/app.js
@@ -2,13 +2,14 @@ define([
 	'app/views/app',
 	'app/routers/router',
 	'app/models/app',
-	'app/collections/days'
-], function (AppView, Router, AppModel, DaysCollection) {
+	'app/collections/days',
+	'app/collections/places'
+], function (AppView, Router, AppModel, DaysCollection, PlacesCollection) {
 
 		'use strict';
 
 		var initialize = function() {
-			var appModel = new AppModel();
+			var appModel = new AppModel({id:1});
 
 			var appView = new AppView({model: appModel});
 			$('body').append(appView.render().el);
@@ -16,7 +17,17 @@ define([
 			var router = new Router(appView);
 			Backbone.history.start();
 
-			var daysCollection = new DaysCollection([], {
+			appModel.fetch();
+
+			var placesCollection = new PlacesCollection([]);
+			placesCollection.fetch();
+
+			window.debug = {
+				settings: appModel,
+				places: placesCollection
+			}
+
+			/*var daysCollection = new DaysCollection([], {
 				//dynamic and calls in many differenct forecasts
 				url: 'http://api.wunderground.com/api/4471fe16425adcfb/forecast/q/IT/Magenta.json'
 			});
@@ -28,7 +39,7 @@ define([
 				error: function (collection, response, options) {
 					console.log('error');
 				}
-			});
+			});*/
 		};
 
 		return {
