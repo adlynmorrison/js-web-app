6d9300a28684f744a58d4d9a47c36a4e79a3abef
Adlyn Morrison <adlynm@gmail.com>
2015-02-14 22:12:07 -0600

    added data function and another default

--- web-app-final1/js/app/models/day.js
+++ web-app-final1/js/app/models/day.js
@@ -6,12 +6,23 @@ define([
 
 	var DayModel = Backbone.Model.extend({
 		defaults: {
+			'conditions': null,
 			'highCelsius': null,
 			'lowCelsius': null,
 			'highFahrenheit': null,
 			'lowFahrenheit': null,
 			'icon_url': ''
-		}
+		},
+		parse: function (data) {
+			var map = {
+				'conditions': data.conditions,
+				'highCelsius': data.high.celsius,
+				'lowCelsius': data.low.celsius,
+				'highFahrenheit': data.high.fahrenheit,
+				'lowFahrenheit': data.low.fahrenheit,
+				'icon_url': data.icon_url
+			}
+		};
 	});
 
 	return DayModel;
