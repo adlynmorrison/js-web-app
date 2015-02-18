d58c20809bd06cae6af7501ea1bee98749c261d8
Adlyn Morrison <adlynm@gmail.com>
2015-02-17 23:01:58 -0600

    added folder

---
+++ web-app-final2/js/app/templates.js
@@ -0,0 +1,38 @@
+define([
+	'underscore'
+], function (_){
+	//template objects
+	var Templates = {};
+
+	Templates['places'] = [
+		'<div class="panel panel-info">',
+			'<div class="panel-heading">',
+				'<h3 class="panel-title">',
+					'<%= name %> - <%= countryCode %>',
+				'<h3>',
+			'</div>',
+			'<div class="panel-body">',
+				'Panel content',
+			'</div>',
+		'</div>'
+	//build as array then build as string
+	].join('');
+
+	Templates['day'] = [
+		'<div>',
+			'<img src="<%= icon_url %>" alt="" class="img-thumbnail">',
+			'<br /><%= conditions %>',
+		'</div>'
+	].join('');
+
+	//loop throuhg templates
+	for (var tmpl in Templates) {
+		//on on properties
+		if(Templates.hasOwnProperty(tmpl)){
+			//compiles each of the strings
+			Templates[tmpl] = _.template(Templates[tmpl]);
+		}
+	}
+
+	return Templates;
+});
